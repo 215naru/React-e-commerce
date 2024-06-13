4bf091996b3b5c8953d39d430a4a383920859d07
nimer <naru_1421@hotmail.com>
2024-06-12 20:12:52 -0500

    Agregue props para CartCard

--- src/views/Cart.tsx
+++ src/views/Cart.tsx
@@ -10,7 +10,7 @@ export default function Cart() {
             <NavBar />
             <Hero first = "mi" second = "carrito" />
             <main>
-                <CartCard/>
+                <CartCard title="iPad Pro 12.9" description="The iPad Pro 12.9 is a stunning piece of technology, boasting a large 12.9-inch Retina display with ProMotion technology. With 256GB of storage, this iPad provides ample space for all your files, apps, and multimedia content. The sleek and slim design, combined with the silver color, gives it a sophisticated look. Enjoy seamless connectivity with the WiFi feature. Capture your memorable moments with the high-quality camera and relive them on the impressive screen. Whether you're a professional artist, student, or just someone who appreciates cutting-edge technology, the iPad Pro 12.9 is a versatile device that meets all your needs." colors="Silver" units="2"/>
                 <CartResume/>
             </main>
             <Footer />
