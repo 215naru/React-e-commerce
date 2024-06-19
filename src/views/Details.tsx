fea8bebb20095351cc69259296c746a81741bf7e
nimer <naru_1421@hotmail.com>
2024-06-18 20:35:26 -0500

    Se consume json para la vista Details

--- src/views/Details.tsx
+++ src/views/Details.tsx
@@ -1,7 +1,8 @@
 import NavBar from "../components/NavBar"
 import Footer from "../components/Footer"
-import products from "../assets/products"
 import { useParams } from "react-router-dom"
+import axios from "axios"
+import { useState, useEffect } from "react"
 import Hero from "../components/Hero"
 import Thumbs from "../components/Thumbs"
 import Description from "../components/Description"
@@ -11,8 +12,29 @@ import Product from "../interfaces/Product"
 
 export default function Details() {
   const {id} = useParams();
-  const product: Product = products.find((each)=> each.id === id);
-  const onsale: Product[] = products.filter((each) => each.onsale);
+  const [product,setProduct] = useState<Product>({
+    id: "",
+    title: "",
+    price: 0,
+    images: [],
+    colors: [],
+  });
+  const [onsale, setOnSale] = useState<Product[]>([]);
+  useEffect(()=>{
+    axios("/products.json")
+    .then((res)=>{
+      const products: Array<Product> = res.data;
+      const detailProduct: Product | undefined = products.find(
+        (each)=> each.id === id
+      );
+      detailProduct && setProduct(detailProduct);
+      const filterProducts: Array<Product> = products.filter(
+        (each)=>each.onsale
+      );
+      filterProducts.length > 0 && setOnSale(filterProducts);
+    })
+    .catch((err)=>console.log(err));
+  },[id]);
   return (
     <>
       <NavBar/>
